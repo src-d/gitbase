ffb75b9dee0dce873cda4d1e296ac021c3c15178
Máximo Cuadros <mcuadros@gmail.com>
2016-10-21 15:22:11 +0200

    git: change NewDatabase, now accepts a git.Repository, tests now using a fixture (#24)

--- git/commits.go
+++ git/commits.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/mvader/gitql/sql"
+
 	"gopkg.in/src-d/go-git.v4"
 )
 
--- git/commits_test.go
+++ git/commits_test.go
@@ -4,25 +4,35 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/mvader/gitql/sql"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/fixtures"
 )
 
 func TestCommitsRelation(t *testing.T) {
 	assert := assert.New(t)
-	var db sql.Database = NewDatabase("https://github.com/smola/galimatias.git")
+
+	f := fixtures.Basic().One()
+	r, err := git.NewFilesystemRepository(f.DotGit().Base())
+	assert.Nil(err)
+
+	db := NewDatabase("foo", r)
 	assert.NotNil(db)
+
 	relations := db.Relations()
 	rel, ok := relations[commitsRelationName]
 	assert.True(ok)
 	assert.NotNil(rel)
 	assert.Equal(commitsRelationName, rel.Name())
 	assert.Equal(0, len(rel.Children()))
+
 	iter, err := rel.RowIter()
 	assert.Nil(err)
 	assert.NotNil(iter)
+
 	row, err := iter.Next()
 	assert.Nil(err)
 	assert.NotNil(row)
+
 	fields := row.Fields()
 	assert.NotNil(fields)
 	assert.IsType("", fields[1])
