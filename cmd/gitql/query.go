1d8c7ed8ecd4f18cea7a0fb591f9fec031a02873
Santiago M. Mola <santi@mola.io>
2016-10-21 17:27:50 +0200

    Add engine and integrate CLI (#31)
    * Add Engine.
    
    * This is the main entry point.
    
    * integrate command
    
    * remove debug left overs

--- cmd/gitql/query.go
+++ cmd/gitql/query.go
@@ -6,10 +6,13 @@ import (
 	"os"
 	"path/filepath"
 
-	gitql "github.com/mvader/gitql/git"
+	gitqlgit "github.com/mvader/gitql/git"
 	"github.com/mvader/gitql/sql"
 
 	"gopkg.in/src-d/go-git.v4"
+	"github.com/mvader/gitql"
+	"io"
+	"github.com/olekukonko/tablewriter"
 )
 
 type CmdQuery struct {
@@ -75,7 +78,7 @@ func (c *CmdQuery) buildDatabase() error {
 	c.print("current HEAD %q\n", head.Hash())
 
 	name := filepath.Base(filepath.Join(c.Path, ".."))
-	c.db = gitql.NewDatabase(name, c.r)
+	c.db = gitqlgit.NewDatabase(name, c.r)
 	return nil
 }
 
@@ -83,7 +86,41 @@ func (c *CmdQuery) executeQuery() error {
 	c.print("executing %q at %q\n", c.Args.SQL, c.db.Name())
 
 	fmt.Println(c.Args.SQL)
+	e := gitql.New()
+	e.AddDatabase(c.db)
+	schema, iter, err := e.Query(c.Args.SQL)
+	if err != nil {
+		return err
+	}
+
+	c.printQuery(schema, iter)
+
 	return nil
+}
+
+func (c *CmdQuery) printQuery(schema sql.Schema, iter sql.RowIter) {
+	w := tablewriter.NewWriter(os.Stdout)
+	headers := []string{}
+	for _, f := range schema {
+		headers = append(headers, f.Name)
+	}
+	w.SetHeader(headers)
+	for {
+		row, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+		rowStrings := []string{}
+		for _, v := range row.Fields() {
+			rowStrings = append(rowStrings, fmt.Sprintf("%v", v))
+		}
+		w.Append(rowStrings)
+	}
+	w.Render()
 }
 
 func findDotGitFolder(path string) (string, error) {
