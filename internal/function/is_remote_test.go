130bd9b49ea0b46d2944243770df15f86bc267dd
Miguel Molina <miguel@erizocosmi.co>
2018-02-21 12:01:46 +0100

    internal/function: implement is_remote function
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

---
+++ internal/function/is_remote_test.go
@@ -0,0 +1,40 @@
+package function
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/src-d/go-mysql-server.v0/sql"
+	"gopkg.in/src-d/go-mysql-server.v0/sql/expression"
+)
+
+func TestIsRemote(t *testing.T) {
+	f := NewIsRemote(expression.NewGetField(0, sql.Text, "name", true))
+
+	testCases := []struct {
+		name     string
+		row      sql.Row
+		expected bool
+		err      bool
+	}{
+		{"null", sql.NewRow(nil), false, false},
+		{"not a branch", sql.NewRow("foo bar"), false, false},
+		{"not remote branch", sql.NewRow("refs/heads/foo"), false, false},
+		{"remote branch", sql.NewRow("refs/remotes/foo/bar"), true, false},
+		{"mismatched type", sql.NewRow(1), false, true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			val, err := f.Eval(tt.row)
+			if tt.err {
+				require.Error(err)
+				require.True(sql.ErrInvalidType.Is(err))
+			} else {
+				require.NoError(err)
+				require.Equal(tt.expected, val)
+			}
+		})
+	}
+}
