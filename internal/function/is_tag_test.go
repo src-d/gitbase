2d1f332d9909a61ad8145d8742bfdf127fa915b3
Miguel Molina <miguel@erizocosmi.co>
2018-02-21 12:01:37 +0100

    internal/function: implement is_tag function
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

---
+++ internal/function/is_tag_test.go
@@ -0,0 +1,40 @@
+package function
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/src-d/go-mysql-server.v0/sql"
+	"gopkg.in/src-d/go-mysql-server.v0/sql/expression"
+)
+
+func TestIsTag(t *testing.T) {
+	f := NewIsTag(expression.NewGetField(0, sql.Text, "name", true))
+
+	testCases := []struct {
+		name     string
+		row      sql.Row
+		expected bool
+		err      bool
+	}{
+		{"null", sql.NewRow(nil), false, false},
+		{"not a ref name", sql.NewRow("foo bar"), false, false},
+		{"not a tag ref", sql.NewRow("refs/heads/v1.x"), false, false},
+		{"a tag", sql.NewRow("refs/tags/v1.0.0"), true, false},
+		{"mismatched type", sql.NewRow(1), false, true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			val, err := f.Eval(tt.row)
+			if tt.err {
+				require.Error(err)
+				require.True(sql.ErrInvalidType.Is(err))
+			} else {
+				require.NoError(err)
+				require.Equal(tt.expected, val)
+			}
+		})
+	}
+}
