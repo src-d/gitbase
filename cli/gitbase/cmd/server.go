2a13758ae982b7ecfb916ab0ca44592cb14c19e4
Máximo Cuadros <mcuadros@gmail.com>
2018-04-24 12:24:49 +0200

    cli: small refactor to align the other products
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

--- cli/gitbase/cmd/server.go
+++ cli/gitbase/cmd/server.go
@@ -1,34 +1,33 @@
-package main
+package cmd
 
 import (
 	"net"
-	"os"
 	"strconv"
-
-	"github.com/sirupsen/logrus"
 
 	"github.com/src-d/gitbase"
 	"github.com/src-d/gitbase/internal/function"
 	"github.com/src-d/gitbase/internal/rule"
 
+	"github.com/sirupsen/logrus"
 	sqle "gopkg.in/src-d/go-mysql-server.v0"
 	"gopkg.in/src-d/go-mysql-server.v0/server"
 	"gopkg.in/src-d/go-vitess.v0/mysql"
 )
 
-// Squashing tables and pushing down join conditions is still a work in
-// progress and unstable. To enable it, the UNSTABLE_SQUASH_ENABLE must
-// not be empty.
-var enableUnstableSquash = os.Getenv("UNSTABLE_SQUASH_ENABLE") != ""
+const (
+	ServerDescription = "Starts a gitbase server instance"
+	ServerHelp        = ServerDescription + "\n\n" +
+		"The squashing tables and pushing down join conditions is still a\n" +
+		"work in progress and unstable,disable by default can be enabled\n" +
+		"using a not empty value at UNSTABLE_SQUASH_ENABLE env variable.\n\n" +
+		"By default when gitbase encounters and error in a repository it\n" +
+		"stops the query. With GITBASE_SKIP_GIT_ERRORS variable it won't\n" +
+		"complain and just skip those rows or repositories."
+)
 
-// By default when gitbase encounters and error in a repository it stops
-// the query. With this parameter it won't complain and just skip those
-// rows or repositories.
-var skipGitErrors = os.Getenv("GITBASE_SKIP_GIT_ERRORS") != ""
-
-type cmdServer struct {
-	Verbose bool `short:"v" description:"Activates the verbose mode"`
-
+// Server represents the `server` command of gitbase cli tool.
+type Server struct {
+	Verbose  bool     `short:"v" description:"Activates the verbose mode"`
 	Git      []string `short:"g" long:"git" description:"Path where the git repositories are located, multiple directories can be defined"`
 	Siva     []string `long:"siva" description:"Path where the siva repositories are located, multiple directories can be defined"`
 	Host     string   `short:"h" long:"host" default:"localhost" description:"Host where the server is going to listen"`
@@ -36,37 +35,23 @@ type cmdServer struct {
 	User     string   `short:"u" long:"user" default:"root" description:"User name used for connection"`
 	Password string   `short:"P" long:"password" default:"" description:"Password used for connection"`
 
+	// UnstableSquash quashing tables and pushing down join conditions is still
+	// a work in progress and unstable. To enable it, the UNSTABLE_SQUASH_ENABLE
+	// must not be empty.
+	UnstableSquash bool
+	// IgnoreGitErrors by default when gitbase encounters and error in a
+	// repository it stops the query. With this parameter it won't complain and
+	// just skip those rows or repositories.
+	SkipGitErrors bool
+
 	engine *sqle.Engine
 	pool   *gitbase.RepositoryPool
 	name   string
 }
 
-func (c *cmdServer) buildDatabase() error {
-	if c.engine == nil {
-		c.engine = sqle.New()
-	}
-
-	c.pool = gitbase.NewRepositoryPool()
-
-	if err := c.addDirectories(); err != nil {
-		return err
-	}
-
-	c.engine.AddDatabase(gitbase.NewDatabase(c.name))
-	logrus.WithField("db", c.name).Debug("registered database to catalog")
-
-	c.engine.Catalog.RegisterFunctions(function.Functions)
-	logrus.Debug("registered all available functions in catalog")
-
-	if enableUnstableSquash {
-		logrus.Warn("unstable squash tables rule is enabled")
-		c.engine.Analyzer.AddRule(rule.SquashJoinsRule, rule.SquashJoins)
-	}
-
-	return nil
-}
-
-func (c *cmdServer) Execute(args []string) error {
+// Execute starts a new gitbase server based on provided configuration, it
+// honors the go-flags.Commander interface.
+func (c *Server) Execute(args []string) error {
 	if c.Verbose {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
@@ -90,7 +75,7 @@ func (c *cmdServer) Execute(args []string) error {
 		},
 		c.engine,
 		gitbase.NewSessionBuilder(c.pool,
-			gitbase.WithSkipGitErrors(skipGitErrors),
+			gitbase.WithSkipGitErrors(c.SkipGitErrors),
 		),
 	)
 	if err != nil {
@@ -101,7 +86,32 @@ func (c *cmdServer) Execute(args []string) error {
 	return s.Start()
 }
 
-func (c *cmdServer) addDirectories() error {
+func (c *Server) buildDatabase() error {
+	if c.engine == nil {
+		c.engine = sqle.New()
+	}
+
+	c.pool = gitbase.NewRepositoryPool()
+
+	if err := c.addDirectories(); err != nil {
+		return err
+	}
+
+	c.engine.AddDatabase(gitbase.NewDatabase(c.name))
+	logrus.WithField("db", c.name).Debug("registered database to catalog")
+
+	c.engine.Catalog.RegisterFunctions(function.Functions)
+	logrus.Debug("registered all available functions in catalog")
+
+	if c.UnstableSquash {
+		logrus.Warn("unstable squash tables rule is enabled")
+		c.engine.Analyzer.AddRule(rule.SquashJoinsRule, rule.SquashJoins)
+	}
+
+	return nil
+}
+
+func (c *Server) addDirectories() error {
 	if len(c.Git) == 0 && len(c.Siva) == 0 {
 		logrus.Error("At least one git folder or siva folder should be provided.")
 	}
@@ -121,12 +131,12 @@ func (c *cmdServer) addDirectories() error {
 	return nil
 }
 
-func (c *cmdServer) addGitDirectory(folder string) error {
+func (c *Server) addGitDirectory(folder string) error {
 	logrus.WithField("dir", c.Git).Debug("git repositories directory added")
 	return c.pool.AddDir(folder)
 }
 
-func (c *cmdServer) addSivaDirectory(folder string) error {
+func (c *Server) addSivaDirectory(folder string) error {
 	logrus.WithField("dir", c.Git).Debug("siva repositories directory added")
 	return c.pool.AddSivaDir(folder)
 }
