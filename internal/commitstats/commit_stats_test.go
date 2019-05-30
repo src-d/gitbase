ead93a35b15962607c8ad39768a337f7cfe96f69
Máximo Cuadros <mcuadros@gmail.com>
2019-05-30 22:24:52 +0200

    commitstats: Calculate function
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

---
+++ internal/commitstats/commit_stats_test.go
@@ -0,0 +1,132 @@
+package commitstats
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/src-d/go-git-fixtures.v3"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/cache"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/storage/filesystem"
+)
+
+func TestCalculate(t *testing.T) {
+	err := fixtures.Init()
+	require.NoError(t, err)
+
+	defer func() {
+		err := fixtures.Clean()
+		require.NoError(t, err)
+	}()
+
+	tests := map[string]struct {
+		fixture  *fixtures.Fixture
+		from     plumbing.Hash
+		to       plumbing.Hash
+		expected *CommitStats
+	}{
+		"basic": {
+			fixture: fixtures.ByURL("https://github.com/src-d/go-git.git").One(),
+			to:      plumbing.NewHash("d2d68d3413353bd4bf20891ac1daa82cd6e00fb9"),
+			expected: &CommitStats{
+				Files:   23,
+				Code:    KindStats{Additions: 414, Deletions: 264},
+				Blank:   KindStats{Additions: 42, Deletions: 65},
+				Comment: KindStats{Additions: 2, Deletions: 337},
+				Total:   KindStats{Additions: 458, Deletions: 666},
+			},
+		},
+		"orphan": {
+			fixture: fixtures.Basic().One(),
+			to:      plumbing.NewHash("b029517f6300c2da0f4b651b8642506cd6aaf45d"),
+			expected: &CommitStats{
+				Files: 2,
+				Other: KindStats{Additions: 22, Deletions: 0},
+				Total: KindStats{Additions: 22, Deletions: 0},
+			},
+		},
+		"other": {
+			fixture: fixtures.Basic().One(),
+			to:      plumbing.NewHash("b8e471f58bcbca63b07bda20e428190409c2db47"),
+			expected: &CommitStats{
+				Files: 1,
+				Other: KindStats{Additions: 1, Deletions: 0},
+				Total: KindStats{Additions: 1, Deletions: 0},
+			},
+		},
+		"binary": {
+			fixture: fixtures.Basic().One(),
+			to:      plumbing.NewHash("35e85108805c84807bc66a02d91535e1e24b38b9"),
+			expected: &CommitStats{
+				Files: 1,
+			},
+		},
+		"vendor": {
+			fixture: fixtures.Basic().One(),
+			to:      plumbing.NewHash("6ecf0ef2c2dffb796033e5a02219af86ec6584e5"),
+			expected: &CommitStats{
+				Files: 1,
+			},
+		},
+		"with_from": {
+			fixture:  fixtures.Basic().One(),
+			to:       plumbing.NewHash("6ecf0ef2c2dffb796033e5a02219af86ec6584e5"),
+			from:     plumbing.NewHash("6ecf0ef2c2dffb796033e5a02219af86ec6584e5"),
+			expected: &CommitStats{},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			require := require.New(t)
+
+			r, err := git.Open(filesystem.NewStorage(test.fixture.DotGit(), cache.NewObjectLRUDefault()), nil)
+			require.NoError(err)
+
+			to, err := r.CommitObject(test.to)
+			require.NoError(err)
+
+			var from *object.Commit
+			if !test.from.IsZero() {
+				from, err = r.CommitObject(test.from)
+				require.NoError(err)
+			}
+
+			stats, err := Calculate(r, from, to)
+			require.NoError(err)
+
+			assert.Equal(t, test.expected, stats)
+		})
+	}
+}
+
+func TestNewFileStats(t *testing.T) {
+	require := require.New(t)
+
+	err := fixtures.Init()
+	require.NoError(err)
+
+	defer func() {
+		err := fixtures.Clean()
+		require.NoError(err)
+	}()
+
+	f := fixtures.Basic().One()
+
+	r, err := git.Open(filesystem.NewStorage(f.DotGit(), cache.NewObjectLRUDefault()), nil)
+	require.NoError(err)
+
+	b, err := r.BlobObject(plumbing.NewHash("9a48f23120e880dfbe41f7c9b7b708e9ee62a492"))
+	require.NoError(err)
+
+	fs, err := newFileStats(b, "PHP")
+	require.NoError(err)
+
+	require.Equal(17, fs["}"].Count)
+	require.Equal(Code, fs["}"].Kind)
+	require.Equal(10, fs["*/"].Count)
+	require.Equal(Comment, fs["*/"].Kind)
+}
